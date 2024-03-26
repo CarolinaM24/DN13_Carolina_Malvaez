9e239334bbac518723c47369fa143ef1d8661240
CarolinaM24 <L202007009@vbravo.tecnm.mx>
2024-03-25 20:41:43 -0600

    T4
    Se agrega la Tarea 4

---
+++ M2_WebDevelopment/T4/DataTypes/DataTypes.Web/wwwroot/DataTypes.js
@@ -0,0 +1,21 @@
+﻿var element = document.getElementById("example");
+var textExample = "thi is a text";
+var aNumber = 10.33;
+
+var complexObject = {
+    name: "Israel",
+    lastName: "Perez",
+    age: 35
+};
+var text1 = "text";
+var text2 = 'text';
+var ul = "<ul>";
+
+//for (var i = 0; i < textExample.length; i++) {
+  //  ul += ("<li>" + textExample[i] + "</li>");
+//}
+//ul += "</ul>";
+var date = "1333333"
+element.innerHTML = typeof (parseInt( date));
+
+
---
+++ M2_WebDevelopment/T4/DataTypes/DataTypes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            string text = "This is a string";
+            int age = 35;
+            DateTime now = DateTime.Now;
+            double amount = 0;
+
+            string string2 = text + " a second string" + age + now + amount;
+
+            string string3 = string.Format("The age is {0}, the tame is {1}, and amout is {2} ", age, now, amount);
+
+            string string4 = $"The age is {age}, the tame is {now}, and amout is {amount}";
+
+            char sampleChar = 'C';
+
+            char[] arrayChar = string4.ToCharArray();
+
+            amount = (double) 10 / (double) 3;
+
+            DateTime dateTime = new DateTime(2000, 1, 1);
+
+            TimeSpan timeStampi = now - dateTime;
+            //var otherText = "this is a text";
+            Console.WriteLine(timeStampi.TotalMicroseconds);
+
+            string test = "15";
+
+            string booleanValue = "True";
+
+            bool isTrue = bool.Parse(booleanValue);
+
+            age = int.Parse(test);
+
+            Console.ReadKey();
+
+        }
+    }
+}
