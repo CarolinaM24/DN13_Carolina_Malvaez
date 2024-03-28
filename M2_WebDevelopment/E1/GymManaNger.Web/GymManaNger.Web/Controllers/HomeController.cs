ab82e138be014af8fc33302cb45e2f009fff5f5e
CarolinaM24 <L202007009@vbravo.tecnm.mx>
2024-03-27 22:23:54 -0600

    E1
    Ejercicios 1 del modulo 2

---
+++ M2_WebDevelopment/E1/GymManaNger.Web/GymManaNger.Web/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GymManaNger.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+        
+
+    }
+}
